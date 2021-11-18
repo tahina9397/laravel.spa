3642baccfc7233c9d1ee02733bf81956c680a2a3
tahina9397 <taksbeh@gmail.com>
2021-11-18 15:37:52 +0300

    d

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\API\BookController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +18,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
+Route::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'books', 'middleware' => 'auth:sanctum'], function () {
+    Route::get('/', [BookController::class, 'index']);
+    Route::post('add', [BookController::class, 'add']);
+    Route::get('edit/{id}', [BookController::class, 'edit']);
+    Route::post('update/{id}', [BookController::class, 'update']);
+    Route::delete('delete/{id}', [BookController::class, 'delete']);
 });
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/{any}', function () {
-    return view('layouts.vue');
+    return view('app');
 })->where('any', '.*');
