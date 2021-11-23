5d67f4c340055e7d3af581956d079e91ddf162b5
tahina9397 <taksbeh@gmail.com>
2021-11-23 06:59:15 +0300

    d

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,6 +12,30 @@ window.Vue = require('vue').default;
 import router from './router';
 import App from './App.vue';
 
+/* font */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import {
+    faTimes,
+    faSpinner,
+    faCheck,
+    faUser,
+    faKey,
+    faTrash,
+    faPlus,
+    faLock,
+    faHeart,
+    faMobileAlt,
+    faExclamationTriangle
+} from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faClock, faLightbulb, faCheckCircle } from '@fortawesome/free-regular-svg-icons'
+
+library.add(faTimes, faSpinner, faCheck, faUser, faKey, faTrash, faPlus, faLock, faHeart, faMobileAlt, faExclamationTriangle)
+library.add(faGithub)
+library.add(faClock, faLightbulb, faCheckCircle)
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -33,6 +57,7 @@ import App from './App.vue';
 
 // Components
 Vue.component("LoadingButton", () => import('./components/Auth/LoadingButton.vue'))
+Vue.component('Fa', FontAwesomeIcon)
 
 const app = new Vue({
     router,
